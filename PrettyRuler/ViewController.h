75c2fbd8772ff1820496439c661bccc5ac59aeb6
唐先海 <2846306067@qq.com>
2015-12-12 01:43:00 +0800

    init

---
+++ PrettyRuler/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  PrettyRuler
+//
+//  Created by GXY on 15/12/11.
+//  Copyright © 2015年 Tangxianhai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
